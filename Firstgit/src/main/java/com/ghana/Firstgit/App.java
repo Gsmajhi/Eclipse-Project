7c083a184228bd1a0c4e6b1a66e2afbffcadd5d3
Ghanashyam Majhi <Ghanashyam Majhi@Ghanashyam>
2025-02-16 23:07:56 +0530

    added x and y

--- Firstgit/src/main/java/com/ghana/Firstgit/App.java
+++ Firstgit/src/main/java/com/ghana/Firstgit/App.java
@@ -11,5 +11,8 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("welcome to the show");
         System.out.println("Code by Gsmajhi");
+        int x=90;
+        int y=78;
+        System.out.println(x+y);
     }
 }
