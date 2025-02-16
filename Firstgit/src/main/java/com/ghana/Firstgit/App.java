33dfe424e2b8c5bca8ef4ce79917f5e1c118a5a9
Ghanashyam Majhi <Ghanashyam Majhi@Ghanashyam>
2025-02-16 22:32:10 +0530

    Second commit

---
+++ Firstgit/src/main/java/com/ghana/Firstgit/App.java
@@ -0,0 +1,13 @@
+package com.ghana.Firstgit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
