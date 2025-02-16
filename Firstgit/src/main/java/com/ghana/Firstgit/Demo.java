22e6f846efef4dd5aba75a6c75a18b63aedb2568
Ghanashyam Majhi <Ghanashyam Majhi@Ghanashyam>
2025-02-16 23:00:20 +0530

    fifth commit

--- Firstgit/src/main/java/com/ghana/Firstgit/Demo.java
+++ Firstgit/src/main/java/com/ghana/Firstgit/Demo.java
@@ -2,6 +2,7 @@ package com.ghana.Firstgit;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("hello friends");
+		
+		System.out.println("java program");
 	}
 }
